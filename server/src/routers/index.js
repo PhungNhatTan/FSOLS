94d8e0e1db179ad90d38abfe9029ef1686e6f56b
huynhvanminh <huynhvanminh17082003@gmail.com>
2025-10-07 11:14:06 +0700

    fix: update file index js

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -1,8 +1,8 @@
-import {router} from "express";
-import courseRoutes from "./courseRoute";
+import {Router} from "express";
+import courseRoutes from "./courseRoute.js";
 
-const routers=router();
+const routers=Router();
 
-router.use("/api/course", courseRoutes);
+routers.use("/api/course", courseRoutes);
 
 export default routers;
