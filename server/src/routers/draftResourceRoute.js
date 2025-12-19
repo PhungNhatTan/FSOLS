b0cd89df9b89c0ac957a82dde464adfb2bb2c70a
unknown <phungnhattan0305@gmail.com>
2025-12-19 17:26:19 +0700

    fix: add role enforcement for draft resource handler BE side

--- server/src/routers/draftResourceRoute.js
+++ server/src/routers/draftResourceRoute.js
@@ -1,32 +1,34 @@
 import { Router } from "express";
 import { draftUpload } from "../middleware/multerDraft.js";
 import * as ctrl from "../controllers/courseResource/courseResource.js";
+import authenticate from '../../middleware/auth.js';
+import { authorize } from '../../middleware/role.js';
 
 const router = Router();
 
 router.post(
-  "/:courseId/draft/resource",
+  "/:courseId/draft/resource", authenticate, authorize(["Mentor"]),  
   draftUpload.single("file"),
   ctrl.uploadDraftResource
 );
 
 router.get(
-  "/:courseId/draft/resources",
+  "/:courseId/draft/resources", authenticate, authorize(["Mentor", "Admin", "Moderator"]), 
   ctrl.listDraftResources
 );
 
 router.delete(
-  "/:courseId/draft/resource/:resourceId",
+  "/:courseId/draft/resource/:resourceId",  authenticate, authorize(["Mentor", "Admin", "Moderator"]), 
   ctrl.deleteDraftResource
 );
 
 router.post(
-  "/:courseId/verification-approve",
+  "/:courseId/verification-approve", authenticate, authorize(["Admin", "Moderator"]), 
   ctrl.approveVerification
 );
 
 router.post(
-  "/:courseId/verification-reject",
+  "/:courseId/verification-reject", authenticate, authorize(["Admin", "Moderator"]),
   ctrl.rejectVerification
 );
 
