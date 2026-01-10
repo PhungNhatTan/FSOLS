010da19e6c91d18d65761d1eecc1a9bd742bcf70
unknown <phungnhattan0305@gmail.com>
2026-01-10 23:26:22 +0700

    fix: remove client-related code from server index.js

--- server/index.js
+++ server/index.js
@@ -1,27 +1,5 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import app from "./src/app.js";
 import { PORT } from "./src/config/server.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-console.log("BOOT from", __filename);
-console.log("DIR is", __dirname);
-
-// Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  const clientBuildPath = path.join(__dirname, "../client/dist");
-  app.use(express.static(clientBuildPath));
-
-  app.get("*", (req, res, next) => {
-    if (req.originalUrl.startsWith("/assets/")) {
-      return next();
-    }
-    res.sendFile(path.join(clientBuildPath, "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
