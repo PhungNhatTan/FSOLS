afcab4547062fec478f91879ae37f2fec288261e
unknown <phungnhattan0305@gmail.com>
2026-01-13 23:04:37 +0700

    fix: schema change

--- server/prisma/migrations/20260113152704_extend_text_fields/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE `examanswer` MODIFY `AnswerText` TEXT NOT NULL;
-
--- AlterTable
-ALTER TABLE `questionbank` MODIFY `QuestionText` TEXT NOT NULL,
-    MODIFY `Answer` TEXT NULL;
-
--- AlterTable
-ALTER TABLE `studentanswer` MODIFY `Answer` TEXT NULL;
