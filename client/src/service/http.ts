52af555ffff9db652dc256b96797646b59269cbd
unknown <phungnhattan0305@gmail.com>
2026-01-13 23:21:27 +0700

    fix: fix http.ts using local

--- client/src/service/http.ts
+++ client/src/service/http.ts
@@ -5,7 +5,7 @@ import axios, {
 } from "axios";
 
 const http: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
 });
 
